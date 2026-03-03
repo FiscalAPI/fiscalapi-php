7cf121b7a89ca298aba5f5e4d092c5380d1c79e4
Jose Antonio Medina <medina.octubre95@live.com.mx>
2026-03-02 21:30:59 -0600

    Updated page size for stamp interface service

--- src/Services/TaxFileServiceInterface.php
+++ src/Services/TaxFileServiceInterface.php
@@ -17,7 +17,7 @@ interface TaxFileServiceInterface extends FiscalApiServiceInterface
      * @param int $pageSize Tamaño de página
      * @return FiscalApiHttpResponseInterface
      */
-    public function list(int $pageNumber = 1, int $pageSize = 20): FiscalApiHttpResponseInterface;
+    public function list(int $pageNumber = 1, int $pageSize = 10): FiscalApiHttpResponseInterface;
 
     /**
      * Obtiene un archivo fiscal por su ID
